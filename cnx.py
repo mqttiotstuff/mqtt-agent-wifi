e2da50e5afef8e2541b82c228f55172d68d408ff
Patrice Freydiere <frett27@gmail.com>
2020-11-08 14:34:36 +0100

    fix python 3, add freebox connection

---
+++ cnx.py
@@ -0,0 +1,39 @@
+
+#
+# Integration test for freebox service in dom
+#     using this url for api description : http://dev.freebox.fr/sdk/os/wifi/
+#     reuse the code for freebox connection : http://www.manatlan.com/blog/freeboxv6_api_v3_avec_python
+#
+#
+
+import freebox
+import json
+
+# read the authorize_result file
+
+app_id = "fr.freebox.dom" 
+j = json.load(open("result_auth"))
+app_token = str(j["result"]["app_token"]) # freebox don't like unicode
+
+# print app_token
+
+
+f = freebox.FbxApp(app_id,app_token)
+
+print f.dir()
+
+def listActualConnectedDevices():
+    # list connected devices
+    ap = f.com("wifi/ap/0/stations")['result']
+    # extract the mac / name
+    couplelist = map(lambda x:(x['mac'],x['hostname']), ap)
+    return couplelist
+
+
+# nintendo yann : (u'9C:E6:35:ED:CF:7A', u'Nintendo 3DS')
+# nintendo quentin : (u'E0:0C:7F:69:41:02', u'Nintendo 3DS')
+# tel pfr : (u'24:4B:81:C3:B9:9B', u'android-c3aa00f846d21a05')
+# tel corinne :  (u'50:F0:D3:D9:81:8C', u'android-d244f64c376175fa')
+
+
+ 
---
+++ connection_agent.py
@@ -0,0 +1,83 @@
+
+#
+# MQTT agent to measure the freebox wifi device emitting power
+# permit to interpolate the device position
+#
+
+# 2020-11 migrting to python 3
+
+import paho.mqtt.client as mqtt
+import random
+import time
+import re
+import configparser
+import os.path
+
+import sys
+import traceback
+
+config = configparser.RawConfigParser()
+
+
+CONNECTION_ROOT = "home/agents/freeboxconnection"
+
+
+import freebox
+import json
+import time
+import string
+
+
+#############################################################
+## MAIN
+
+conffile = os.path.expanduser('~/.mqttagents.conf')
+if not os.path.exists(conffile):
+   raise Exception("config file " + conffile + " not found")
+
+config.read(conffile)
+
+username = config.get("agents","username")
+password = config.get("agents","password")
+mqttbroker = config.get("agents","mqttbroker")
+
+client2 = mqtt.Client()
+
+# client2 is used to send time oriented messages, without blocking the receive one
+client2.username_pw_set(username, password)
+client2.connect(mqttbroker, 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+
+client2.loop_start()
+
+app_id = "fr.freebox.dom" 
+j = json.load(open("result_auth"))
+app_token = str(j["result"]["app_token"]) # freebox don't like unicode
+
+f = freebox.FbxApp(app_id,app_token)
+
+cnx_reconnect = 100
+
+while True:
+    time.sleep(1)
+    try:
+    # reconnect if needed
+        cnx_reconnect = cnx_reconnect - 1
+        if cnx_reconnect < 0:
+            f = freebox.FbxApp(app_id,app_token)
+            cnx_reconnect = 100
+
+    # query the box
+        ap = f.com("/connection")['result']
+
+        for k,v in ap.items():
+            client2.publish(CONNECTION_ROOT + "/" + k , str(v))
+
+    except Exception as e:
+        traceback.print_exc()
+
